b59e3f148a9bc28487338de99950e2215351fb8f
mitchell <none@none>
2010-04-16 13:39:38 -0400

    Rewrote lexer infrastructure, documentation, and lexers.

--- lexers/container.lua
+++ lexers/container.lua
@@ -1,6 +1,4 @@
 -- Copyright 2006-2010 Mitchell Foral mitchell<att>caladbolg.net. See LICENSE.
 -- Container LPeg lexer
 
-module(..., package.seeall)
-
-function Lex(input) return {} end
+module(...)
